d1fb98095cd4ae579915e9840700e31bcae726f3
Christof Donat <cd@okunah.de>
2026-04-20 17:14:18 +0200

    unify name from "well_known" to "webfinger"

--- src/profed/components/api/app.py
+++ src/profed/components/api/app.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 from fastapi import FastAPI
-from .s2s.webfinger import router as well_known
+from .s2s.webfinger import router as webfinger
 from .s2s.actor     import router as actor
 from .s2s.inbox     import router as inbox
 from .s2s.outbox    import router as outbox
@@ -13,7 +13,7 @@ def create_app(config):
 
     deactivate_routers = config.get("deactivate_routers", "").split()
     init_routers = [rt
-                    for name, rt in (("s2s_well_known", well_known.router),
+                    for name, rt in (("s2s_webfinger", webfinger.router),
                                      ("s2s_actor", actor.router),
                                      ("s2s_inbox", inbox.router),
                                      ("s2s_outbox", outbox.router))
