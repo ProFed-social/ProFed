0575c48b69854d7f1c325603e9f72e1eea7254ed
Christof Donat <cd@okunah.de>
2026-04-23 18:09:39 +0200

    add more logging

--- src/profed/components/activity_delivery/__init__.py
+++ src/profed/components/activity_delivery/__init__.py
@@ -10,17 +10,27 @@ from .projections import (followers_handle_events,
                            keys_handle_events,
                            keys_rebuild)
 from .handler import handle_activities
+
+from logging import Logger
+
+logger = Logger(__name__)
  
  
 async def ActivityDelivery(config: dict) -> None:
+    logger.info("setting up activity delivery")
     await init_storage(config)
+    logger.info("storage has been initialized")
     await asyncio.gather(followers_rebuild(),
                          deliveries_rebuild(),
                          keys_rebuild())
-
+    logger.info("projections have been rebuilt")
 
     asyncio.create_task(followers_handle_events(), name="activity_delivery_followers")
+    logger.info("kicked off follwers handler")
     asyncio.create_task(deliveries_handle_events(), name="activity_delivery_deliveries")
+    logger.info("kicked off deliveries handler")
     asyncio.create_task(keys_handle_events(), name="activity_delivery_keys")
+    logger.info("kicked off keys handler")
     asyncio.create_task(handle_activities(config), name="activity_delivery_handler")
+    logger.info("kicked off activities handler")
 
