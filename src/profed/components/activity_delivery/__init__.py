990c26bf66face7a5a625576c6a69df9a8b97ce3
Christof Donat <cd@okunah.de>
2026-04-23 23:18:21 +0200

    search for bug

--- src/profed/components/activity_delivery/__init__.py
+++ src/profed/components/activity_delivery/__init__.py
@@ -17,6 +17,7 @@ logger = logging.getLogger(__name__)
  
  
 async def ActivityDelivery(config: dict) -> None:
+    await asyncio.sleep(5)
     logger.info("setting up activity delivery")
     await init_storage(config)
     logger.info("storage has been initialized")
