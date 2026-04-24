0f2451b0fd5aaa2cce974534f64c39b7a6274cc9
Christof Donat <cd@okunah.de>
2026-04-24 19:02:49 +0200

    make logging level configurable

--- src/profed/__main__.py
+++ src/profed/__main__.py
@@ -1,10 +1,21 @@
 # Copyright (C) 2026 Christof Donat
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+import logging
+
+ 
+logging.basicConfig(level=logging.WARNING,
+                    format="%(levelname)s %(name)s %(message)s")
+ 
+
 from profed.core.config import config
 from profed.core.component_manager import run
 from profed.core.message_bus import init_message_bus
 
 
 if __name__ == "__main__":
-    run(config(), [init_message_bus])
+    cfg = config()
+    level = cfg.get("logging", {}).get("level", "INFO")
+    logging.getLogger().setLevel(getattr(logging, level.upper()))
+    run(cfg, [init_message_bus])
+
