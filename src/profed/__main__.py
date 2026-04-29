cf11c9d895bb5bc0854a57371e625893befbc55d
Christof Donat <cd@okunah.de>
2026-04-29 17:05:48 +0200

    simplify configuration with default component list

--- src/profed/__main__.py
+++ src/profed/__main__.py
@@ -2,20 +2,30 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 import logging
-
  
 logging.basicConfig(level=logging.WARNING,
                     format="%(levelname)s %(name)s %(message)s")
- 
 
 from profed.core.config import config
 from profed.core.component_manager import run
 from profed.core.message_bus import init_message_bus
 
 
+STANDARD_COMPONENTS = " ".join(("api",
+                                "user_activities",
+                                "activity_delivery",
+                                "follow_handler",
+                                "accept_handler"))
+
+
 if __name__ == "__main__":
     cfg = config()
+
     level = cfg.get("logging", {}).get("level", "INFO")
     logging.getLogger().setLevel(getattr(logging, level.upper()))
+
+    cfg["profed"] = cfg.get("profed", {})
+    cfg["profed"]["run"] = cfg["profed"].get("run", STANDARD_COMPONENTS)
+
     run(cfg, [init_message_bus])
 
