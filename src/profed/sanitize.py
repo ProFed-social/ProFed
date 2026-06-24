88f792fb1456aaba0c7e44a69e99f6c7cd1c15b8
Christof Donat <cd@okunah.de>
2026-06-24 18:52:12 +0200

    remove useless comments

--- src/profed/sanitize.py
+++ src/profed/sanitize.py
@@ -2,22 +2,14 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 import re
-
 import nh3
 
-# Allowlist an Mastodons sanitize_config.rb orientiert (Tags und Link-Protokolle
-# gegen die Quelle verifiziert). Eine Policy, an beiden Grenzen genutzt: am Ingress
-# (importierter/foederierter/eigener Inhalt) und beim Rendern (zweite Verteidigung).
-# sanitize_html ist idempotent, beim Rendern bereits gesaeuberten Inhalts also ein No-op.
 
 _TAGS = {
     "p", "br", "span", "a", "abbr", "del", "pre", "blockquote", "code",
     "b", "strong", "u", "sub", "sup", "i", "em",
     "h1", "h2", "h3", "h4", "h5", "ul", "ol", "li",
 }
-
-# "rel" verwaltet nh3 ueber link_rel; "src" ist bewusst nirgends erlaubt (keine
-# eingebetteten Bilder/Medien im Textfluss).
 _ATTRIBUTES = {
     "a": {"href", "class", "translate"},
     "span": {"class", "translate"},
@@ -26,16 +18,11 @@ _ATTRIBUTES = {
     "ol": {"start", "reversed"},
     "li": {"value"},
 }
-
 _URL_SCHEMES = {
     "http", "https", "dat", "dweb", "ipfs", "ipns",
     "ssb", "gopher", "xmpp", "magnet", "gemini",
 }
-
 _LINK_REL = "nofollow noopener noreferrer"
-
-# Wie Mastodon nur Microformats- (h-/p-/u-/dt-/e-) und mention/hashtag-Klassen behalten;
-# das verhindert UI-Spoofing ueber fremde Klassen.
 _ALLOWED_CLASS = re.compile(r"^(?:h|p|u|dt|e)-|^(?:mention|hashtag)$")
 
 
